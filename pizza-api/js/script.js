5725259f6e6d141d8de40a8efa17dfec71e8e9eb
handrihmw <handrihmw@gmail.com>
2019-02-15 09:18:46 +0700

    Hallo API

---
+++ pizza-api/js/script.js
@@ -0,0 +1,36 @@
+function tampilMenuAll(){
+    $.getJSON('data/pizza.json', function (data){
+        let menu = data.menu;
+        $.each(menu, function (i, data) {
+            $('#daftar-menu').append('<div class="col-md-3"><div class="card mb-3"><img class="card-img-top" src="img/pizza/' + data.gambar + '"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><h5 class="card-title">Rp. '+ data.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
+        });
+    });
+}
+
+tampilMenuAll();
+
+$('.nav-link').on('click', function () {
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
+
+    let kategori = $(this).html();
+    $('h1').html(kategori);
+
+    if( kategori == 'All Menu' ){
+        tampilMenuAll();
+        return;
+    }
+
+    $.getJSON('data/pizza.json', function (data) {
+        let menu = data.menu;
+        let content = '';
+
+        $.each(menu, function (i, data) {
+            if(data.kategori == kategori.toLowerCase()){
+                content += '<div class="col-md-4"><div class="card mb-3"><img class="card-img-top" src="img/pizza/' + data.gambar + '"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><h5 class="card-title">Rp. '+ data.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>';
+            }
+        });
+
+        $('#daftar-menu').html(content);
+    });
+});
